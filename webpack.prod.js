44cd25a40c68d3e65d2178f5fca3460843558ee5
Pawarit Kowitwerathom <pawarit.k@geotalent.co.th>
2018-02-01 09:58:26 +0700

    update config wepack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,10 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common,{
     plugins:[
-        new CleanWebpackPlugin(['public']),//clean folder public
         new UglifyJSPlugin(),// uglify bundle js
-        new HtmlWebpackPlugin({
-            template: 'index.html'  // re-create index.html every build
-        })
     ],
     externals:{
         config: JSON.stringify(require('./config/globalConfig.prod.json'))
